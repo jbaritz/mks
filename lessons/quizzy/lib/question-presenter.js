63c43ab56bb45cf11a92d00dd46b3518d163fac6
Julia Baritz <jbaritz@gmail.com>
2015-01-15 10:48:04 -0600

    added JS practice

--- lessons/quizzy/lib/question-presenter.js
+++ lessons/quizzy/lib/question-presenter.js
@@ -2,14 +2,21 @@
 	window.QuizPresenter = {}
 
 	var $form = $('.questions')
+	var questionTallies = [
+		{id: 1, attempts: 0, correct: 0},
+		{id: 2, attempts: 0, correct: 0}
+			]
+	if (! localStorage.questionTallies){
+		localStorage.setItem("questionTallies", JSON.stringify(questionTallies))
+	}
 
 	QuizPresenter.renderQuizzes = function(){
-		// $form.empty();
+		$form.empty();
 		var questionDivs = Quiz.questions.map(function (q){
 			return $('<div>').addClass("question").append($('<h4>').text(q.question)
 			, q.options.map(function (option){
 					return $('<div>').addClass('option').attr('data-id',q.id).append(
-						$('<input>').attr({type: "radio", name: "question_"+q.id}),
+						$('<input>').attr({type: "radio", id:"option_"+option, name: "question_"+q.id}),
 						$('<label>').text(option)
 					)
 				}) //options map
@@ -20,23 +27,59 @@
 		
 	}//end renderQuizzes
 
-	QuizPresenter.renderQuizzes();
+	QuizPresenter.renderQuizzes();//print quiz
 
-	$form.on('submit', function (e) {
-		e.preventDefault()
-		console.log("You submitted")
-		
-		$form.find('.option:is(selected)').map(function (idx, elem) {
-			console.log(idx, $(elem).attr('data-id'))
-		})
+	$('.quizzy').on('submit', function (e) {
+		e.preventDefault();
+		$('.question-avgs').empty()
+		var name = $("form input[name='student']").val();
+		var sel = $("form input[type='radio']:checked")
+		var qs = Quiz.questions.length;
+		var selected = []
+		for (var i =0; i < qs; i++) {
+			// var ops = Quiz.questions[i].options.length;
+		 //  for(var j = 0; j < ops; j++){
+		  	console.log(sel)
+		  	selected.push(sel[i]["id"])
+		}
+		var ans = []
+		selected.forEach(function (sel){
+			var bits = sel.split("_");
+			ans.push(bits[1]);
+		})	
 		var answers = [
-			{ id: 1, answer: 2 },
+			{ id: 1, answer: 1 },
 			{ id: 2, answer: 0 },
 		]
-		Quiz.calculateScore(answers)
-		// Show score to user
-		return false
-	})
- 
+		var score = 0;
+		for (var i = 0; i < Quiz.questions.length; i++){
+				var qdata = JSON.parse(localStorage.questionTallies)
+				qdata[i].attempts++
+				
+				var correctpos = Quiz.questions[i].answer;
+				if(Quiz.questions[i].options[correctpos] === ans[i]){
+					score++;
+					qdata[i].correct++
+				}
+			localStorage.clear();
+			localStorage.setItem("questionTallies", JSON.stringify(qdata))
+
+			putsAvg(i);
+			}//end for-each-q
+		var scoreDiv = $('<div>')
+		scoreDiv.text(name+"'s Score: " + score)
+		$('form').append(scoreDiv)
+		}) //end on-submit
+
+	function putsAvg(i){
+		
+		var qdata = JSON.parse(localStorage.questionTallies)[i]
+		var percent = parseInt((qdata.correct / qdata.attempts ) * 100)
+		var div = $('<div>').attr("id",i)
+		div.text("Question "+(i+1)+" average: "+percent+"%")
+		$('.question-avgs').append(div)
+
+	}
+
 
 })() //end all
--- lessons/quizzy/lib/question.js
+++ lessons/quizzy/lib/question.js
@@ -16,24 +16,4 @@ window.Quiz = {}
   }
       ];
 
-
-  // Question.add = function (q) {
-  //   // if (! q) {
-  //   //   return { error: 'question_required' }
-  //   // }
-  //   var question = 
-
-  //   questions.push(question);
-  //   qId++;
-  //   // 1. Publish / Subscribe pattern (pubsub)
-  //   // 2. Observer pattern
-
-  //   // Publish event
-  //   // Open-closed principle (sOlid)
-  //   // $(document).trigger('new-question')
-  //   console.log(questions);
-  //   return { success: true }
-  // }
-
-
 })()//end of the whole thing
