3a90697a5ae181c9d4d41268af9e0d6e8a99eda8
Julia Baritz <jbaritz@gmail.com>
2015-01-16 11:00:07 -0600

    quizzy progress

--- lessons/quizzy2/src/quizcontroller.js
+++ lessons/quizzy2/src/quizcontroller.js
@@ -9,7 +9,8 @@
 		correctAnswers: m.prop([
 			 {id: 1, answer: 1},
 			 {id: 2, answer: 0}
-			])
+			]),
+		score: m.prop()
 	}
 
 	QuizApp.controller = function (){
@@ -20,10 +21,11 @@
 			var qs = QuizApp.vm.questions().length
 			for(var i = 0; i < qs; i++){
 				if (QuizApp.vm.correctAnswers()[i]['answer'] === QuizApp.vm.selectedAnswers()[i+1]){
-						score++
+						score++;
 				}
 			}
-			return QuizApp.renderScore(score);
+		
+			QuizApp.vm.score(score);
 		}//end calcScore
 		return ctrl
 	}
@@ -33,7 +35,9 @@
 				[m('label', "Enter your name: "), m('input[type="text"]',{class: "name"}),
 			QuizApp.vm.questions().map(quizView)
 			]),
-			m('button[type="submit"]',{onclick: ctrl.calcScore}, "Submit")]
+			m('button[type="submit"]',{onclick: ctrl.calcScore}, "Submit"),
+			answerStatus()
+		]
 
 		function quizView (qs) {
 			return m('.question', [
@@ -41,25 +45,36 @@
 				qs.options.map(radioButtons)
 			])
 
-			function radioButtons (ops, idx) {
-				return m('.option', [
-					m('input[type=radio]', {
-						id: ops,
-						name: "question"+qs.id,
-						onchange: function () {
-							QuizApp.vm.selectedAnswers()[qs.id] = idx
-						}
-					}),
-					m('label', ops)
+		function radioButtons (ops, idx) {
+			return m('.option', [
+				m('input[type=radio]', {
+					id: ops,
+					name: "question"+qs.id,
+					onchange: function () {
+						QuizApp.vm.selectedAnswers()[qs.id] = idx
+					}
+				}),
+				m('label', ops)
 				])
+			}//end radioButtons
+		
+		}//end quizView
+
+		function answerStatus(){
+			if (QuizApp.vm.score() === undefined) {return}
+			else {
+				return m('.quiz', [m('.score', "Your Score: "+ QuizApp.vm.score())]);
 			}
-		}
+		}//end answerStatus
+
 
 	}
 
-	QuizApp.renderScore = function(score){
-		console.log(score);
-		return m('.quiz', [m('.score', "Your Score: "+ score)])
-	}
-
+	Function.prototype.chill = function() {
+	  var fn = this
+	  return function(e) {
+	    e.preventDefault()
+	    return fn()
+	  };
+	};
 })()
